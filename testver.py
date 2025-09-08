5999d56ce2fe3f810fb97eb24e4af98b834aca61
CouMarinX <sunxr1224@mails.jlu.edu.cn>
2025-09-08 21:46:56 +0800

    Remove incomplete test script

--- testver.py
+++
@@ -1,9 +0,0 @@
-import os
-from hanzi_chaizi import HanziChaizi
-import tensorflow as tf
-import tkinter as tk
-from tkinter import messagebox
-from PIL import Image, ImageDraw, ImageFont, ImageTk
-import numpy as np
-import win32clipboard
-from io import BytesIO
